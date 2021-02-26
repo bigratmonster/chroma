b19c8e0a30d5829970ffe07ea30d4e7fb6a5ab8c
EmeraldSnorlax <43452521+EmeraldSnorlax@users.noreply.github.com>
2021-02-26 18:19:16 +0000

    keyboard shortcut to toggle

--- clickthrough.ts
+++ clickthrough.ts
@@ -1,12 +1,20 @@
 const ratImg = document.getElementById('rat-img');
 let chroma = false;
 
-(ratImg as HTMLImageElement).addEventListener('click', () => {
+function toggleChroma(chroma: boolean): boolean {
 	if (!chroma) {
 		(ratImg as HTMLImageElement).src = './bigrat-chroma.gif';
-		chroma = true
+		return true
 	} else {
 		(ratImg as HTMLImageElement).src = './bigrat-static.png';
-		chroma = false
+		return false
 	}
+}
+
+
+(ratImg as HTMLImageElement).addEventListener('click', () => { chroma = toggleChroma(chroma)});
+
+document.addEventListener('keydown', (pressed) => {
+	if(pressed.key == 'j') { chroma = toggleChroma(chroma) }
 });
+
